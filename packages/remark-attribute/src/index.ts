3f12d21075bf3c6ff6eeb0625c6e93903572f874
Koutaro Mukai <mukai.k1011k@outlook.jp>
2026-02-11 16:34:21 +0900

    rewrite

--- packages/remark-attribute/src/index.ts
+++ packages/remark-attribute/src/index.ts
@@ -1,1 +1,29 @@
-export { default as remarkAttribute } from "./lib.ts";
+import type * as unified from "unified";
+import { attributeFromMarkdown } from "mdast-util-attribute";
+import type { Options } from "mdast-util-attribute";
+import { attribute } from "micromark-extension-attribute";
+
+/**
+ * Add support for attributes (`{#id .class key=value}`).
+ *
+ * @param options
+ *   Configuration (optional).
+ * @returns
+ *   Nothing.
+ */
+export function remarkAttribute(
+  this: unified.Processor,
+  options?: Options | null | undefined,
+) {
+  const data = this.data() as Record<string, unknown>;
+
+  const micromarkExtensions =
+    (data.micromarkExtensions as unknown[]) ||
+    (data.micromarkExtensions = [] as unknown[]);
+  const fromMarkdownExtensions =
+    (data.fromMarkdownExtensions as unknown[]) ||
+    (data.fromMarkdownExtensions = [] as unknown[]);
+
+  micromarkExtensions.push(attribute());
+  fromMarkdownExtensions.push(attributeFromMarkdown(options));
+}
